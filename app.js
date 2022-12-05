9024e9dfa793f843fbce2cf325a1c761032deeb0
Nick <ntoukas.nick@gmail.com>
2022-12-05 09:54:39 -0500

    fetch prducts from api

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('products starter');
+const url = `https://course-api.com/javascript-store-products`;
+const productsDOM = document.querySelector('.products-center');
+
+const fetchProducts = async () => {
+  productsDOM.innerHTML = `<div class='loading'></div>`;
+  try {
+    const resp = await fetch(url);
+    const data = await resp.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+    productsDOM.innerHTML = `<p class="error">there was an error</p>`;
+  }
+};
+fetchProducts();
